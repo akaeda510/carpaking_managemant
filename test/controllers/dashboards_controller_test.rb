2942b47cc3ccc90a6a119794aecd0af2edd89a33
akaeda510 <moontype510@i.softbank.jp>
2025-11-24 20:58:50 +0900

    update:dashboard_testを修正

--- test/controllers/dashboards_controller_test.rb
+++ test/controllers/dashboards_controller_test.rb
@@ -1,8 +1,13 @@
 require "test_helper"
 
 class DashboardsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @parking_manager = parking_managers(:one)
+    sign_in @parking_manager
+  end
+
   test "should get show" do
-    get dashboards_show_url
+    get dashboard_url
     assert_response :success
   end
 end
