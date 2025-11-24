e287846e34f70a2c951c2883005a141e13b6e75b
akaeda510 <moontype510@i.softbank.jp>
2025-11-24 20:16:54 +0900

    update:dashboard_testを修正

--- test/controllers/dashboards_controller_test.rb
+++ test/controllers/dashboards_controller_test.rb
@@ -1,8 +1,8 @@
 require "test_helper"
 
 class DashboardsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get dashboards_index_url
+  test "should get show" do
+    get dashboards_show_url
     assert_response :success
   end
 end
