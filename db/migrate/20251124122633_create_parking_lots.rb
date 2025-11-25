cce149eaacdca89eace2feba52ae61806d17941a
akaeda510 <moontype510@i.softbank.jp>
2025-11-25 22:24:02 +0900

    update:routes_parking_lots

--- db/migrate/20251124122633_create_parking_lots.rb
+++ db/migrate/20251124122633_create_parking_lots.rb
@@ -1,10 +1,12 @@
 class CreateParkingLots < ActiveRecord::Migration[8.1]
   def change
     create_table :parking_lots do |t|
-      t.string :name,          null: false, default: ""
-      t.string :address,       null: false, default: ""
-      t.text :description,                  default: ""
-      t.string :total_spaces,  null: false, default: ""
+      t.string :name,               null: false, default: ""
+      t.string :prefecture,         null: false, default: ""
+      t.string :city,               null: false, default: ""
+      t.string :street_address,     null: false, default: ""
+      t.text :description,                       default: ""
+      t.string :total_spaces,       null: false, default: ""
 
       t.references :parking_manager, foreign_key: true
       t.timestamps
