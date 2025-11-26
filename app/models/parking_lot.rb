ee5ac02541ea06072445f0893865972f0d0c73d9
akaeda510 <moontype510@i.softbank.jp>
2025-11-26 21:19:31 +0900

    add:model_parking_lot

--- app/models/parking_lot.rb
+++ app/models/parking_lot.rb
@@ -1,7 +1,9 @@
 class ParkingLot < ApplicationRecord
   validates :name, presence: true, length: { maximum: 40 }
-  validates :address, presence: true, length: { maximum: 140 }
-  validates :desctiption, presence: true, length: { maximum: 65_535 }
-  validates :total_spaces, presence: true, length: { in: 1..2 }, numericality: { only_integer: true }
+  validates :prefecture, presence: true, length: { maximum: 10 }
+  validates :city, presence: true, length: { maximum: 20 }
+  validates :street_address, presence: true, length: { maximum: 50 }
+  validates :description, length: { maximum: 150 }
+  validates :total_spaces, presence: true, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 99 }
   belongs_to :parking_manager
 end
