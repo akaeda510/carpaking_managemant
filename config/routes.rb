9f784d0d17bbdf97fe54dced3229fd7ebaf6ff8a
akaeda510 <moontype510@i.softbank.jp>
2025-11-29 18:34:46 +0900

    update:routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
   }
 
   resource :account, only: [ :show ], controller: "parking_managers", path: "profile", as: :profile
-  resources :parking_lots, only: %i[ new create show index update edit destroy ] do
-  resources :parking_spaces, only: %i[ new create show index update edit destroy ]
+  resources :parking_lots, only: %i[ new create index update edit destroy ] do
+  resources :parking_spaces, only: %i[ new create index ]
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
