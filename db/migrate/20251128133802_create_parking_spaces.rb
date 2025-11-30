bd1ccc9f5f7d516db9783be1409c416ab400738c
akaeda510 <moontype510@i.softbank.jp>
2025-11-30 17:25:06 +0900

    update:migrate_parking_spaces

--- db/migrate/20251128133802_create_parking_spaces.rb
+++ db/migrate/20251128133802_create_parking_spaces.rb
@@ -13,6 +13,5 @@ class CreateParkingSpaces < ActiveRecord::Migration[8.1]
 
       t.index :name
     end
-    add_index :parking_spaces, [ :parking_manager_id, :parking_lot_id ], unique: true
   end
 end
