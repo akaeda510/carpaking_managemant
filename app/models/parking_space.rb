8ac813492d06c0e56e876eca1268bb844dd2b5bc
akaeda510 <moontype510@i.softbank.jp>
2025-11-30 17:15:31 +0900

    update:model_parking_space

--- app/models/parking_space.rb
+++ app/models/parking_space.rb
@@ -4,7 +4,7 @@ class ParkingSpace < ApplicationRecord
   validates :width, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 9.99 }
   validates :length, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 9.99 }
 
-  validates :parking_manager_id, presence: true
   belongs_to :parking_lot
   belongs_to :parking_manager
+
 end
