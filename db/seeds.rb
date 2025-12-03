d20a37920f7d7a47d40f1ce008936b4118eff59e
akaeda510 <moontype510@i.softbank.jp>
2025-12-03 20:45:21 +0900

    reset_seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,26 +6,4 @@
 #
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
-#
-manager = ParkingManager.find_or_create_by!(email: 'test@example.com') do |manager|
-  manager.password = 'password'
-  manager.password_confirmation = 'password'
-
-  manager.first_name = '田中'
-  manager.last_name = '太郎'
-  manager.prefecture = '東京都'
-  manager.city = '品川区'
-  manager.street_address = '西品川'
-  manager.phone_number = '09012345678'
-end
-puts "ParkingManager: created or found."
-
-ParkingLot.find_or_create_by!(name: 'テスト用駐車場') do |lot|
-  lot.parking_manager = manager
-
-  lot.prefecture = '東京都'
-  lot.city = '品川区'
-  lot.street_address = '西品川'
-  lot.total_spaces = 10
-end
-puts "ParkingLot: created or found."
+# end
