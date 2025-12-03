037cf7eaea24ef0a90cd59094c995b0e8d2ab7c7
akaeda510 <moontype510@i.softbank.jp>
2025-12-03 20:47:02 +0900

    update:model_parking_space

--- app/models/parking_space.rb
+++ app/models/parking_space.rb
@@ -1,10 +1,9 @@
 class ParkingSpace < ApplicationRecord
   validates :name, presence: true, uniqueness: true, length: { maximum: 10 }
   validates :description, length: { maximum: 150 }
-  validates :width, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 9.99 }
-  validates :length, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 9.99 }
-
-  enum parking_type: { asphalt: 0, gravel: 1, garage: 2 }
+  validates :width, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 9.9 }
+  validates :length, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 9.9 }
+  validates :height, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 9.9 }
 
   belongs_to :parking_lot
   belongs_to :parking_manager
