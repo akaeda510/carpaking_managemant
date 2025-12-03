2378899852da89eaa3b03065c24ff28f09537eb1
akaeda510 <moontype510@i.softbank.jp>
2025-12-03 21:40:59 +0900

    lint修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
-     
+
 manager = ParkingManager.find_or_create_by!(email: 'test@example.com') do |manager|
   manager.password = 'password'
   manager.password_confirmation = 'password'
