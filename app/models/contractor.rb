123a30132a39b26c4ac9fb01d50eba2bf6578601
akaeda510 <moontype510@i.softbank.jp>
2025-12-04 21:07:44 +0900

    add:model_contractor

---
+++ app/models/contractor.rb
@@ -0,0 +1,16 @@
+class Contractor < ApplicationRecord
+  validates :first_name, presence: true, length: { maximum: 20 }
+  validates :last_name, presence: true, length: { maximum: 20 }
+  # 都道府県
+  validates :prefecture, presence: true, length: { maximum: 10 }
+  # 市区町村
+  validates :city, presence: true, length: { maximum: 20 }
+  # 丁目番地号
+  validates :street_address, presence: true, length: { maximum: 50 }
+  # マンション名、部屋番号等
+  validates :building, length: { maximum: 55 }, allow_nil: true
+  validates :phone_number, presence: true, length: { is: 11 }, numericality: { only_integer: true }, uniqueness: true
+  validates :contact_number, length: { minimum: 10, maximum: 11 }, numericality: { only_integer: true }, allow_nil: true, allow_blank: true
+
+  belongs_to :parking_manager
+end
