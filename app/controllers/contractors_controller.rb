0430d55f23f6f51a9f8215feeb12ce0562f6a332
akaeda510 <moontype510@i.softbank.jp>
2025-12-04 21:34:47 +0900

    add:controller_contractor

---
+++ app/controllers/contractors_controller.rb
@@ -0,0 +1,24 @@
+class ContractorsController < ApplicationController
+  before_action :authenticate_parking_manager!
+
+  def new
+    @contractor = Contractor.new
+  end
+
+  def create
+    @contractor = current.parking_manager.contractor.build(contractor_params)
+    if @contractor.save
+      redirect_to contractors_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def contractor_params
+    params.require(:contractor).permit(
+      :first_name, :last_name, :prefecture, :city, :street_address,
+      :buildint, :phone_number, :contact_number
+    )
+end
