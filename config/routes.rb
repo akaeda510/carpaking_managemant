f9e85b33b008b24eafaa8edb30b26997b6016436
akaeda510 <moontype510@i.softbank.jp>
2025-12-04 22:09:12 +0900

    update:routes_contractor_index

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   resources :parking_spaces, only: %i[ new create index show edit update destroy ]
   end
 
-  resources :contractors, only: %i[ new create ]
+  resources :contractors, only: %i[ new create index ]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
