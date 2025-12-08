96dfe53da51cd37792c5c110f873953ff00f8c6a
akaeda510 <moontype510@i.softbank.jp>
2025-12-08 21:05:42 +0900

    update:controller_dashboard_authorize

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,6 +2,7 @@ class DashboardsController < ApplicationController
   before_action :authenticate_parking_manager!
 
   def show
+    authorize @dashboard
    @parking_manager = current_parking_manager
   end
 end
