1551a44775097aa602bce60dff88c84972af60b2
akaeda510 <moontype510@i.softbank.jp>
2025-12-13 09:10:22 +0900

    update:model_parking_space

--- app/models/parking_space.rb
+++ app/models/parking_space.rb
@@ -5,6 +5,8 @@ class ParkingSpace < ApplicationRecord
   validates :length, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 9.9 }
 
   has_many :contract_parking_spaces, dependent: :destroy
+  has_many :active_contractor_parking_spaces, -> { where('end_date >= ?', Date.current) }, class_name: 'ContractorParkingSpace'
+  has_many :contractor, through: :active_contractor_parking_spaces
 
   belongs_to :parking_lot
   belongs_to :parking_manager
