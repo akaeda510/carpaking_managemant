543d5e3ed0285b55795a8b15b4088f13dba2b9f6
akaeda510 <moontype510@i.softbank.jp>
2025-12-14 16:50:46 +0900

    update:application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-require 'pundit'
+require "pundit"
 
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
