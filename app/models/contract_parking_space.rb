769202bb0334aba020c268cf11ee9e00a7b308f8
akaeda510 <moontype510@i.softbank.jp>
2025-12-14 17:25:38 +0900

    update:model_contract_parking_space_validates

--- app/models/contract_parking_space.rb
+++ app/models/contract_parking_space.rb
@@ -1,6 +1,9 @@
 class ContractParkingSpace < ApplicationRecord
   validates :start_date, presence: true
   validates :end_date, presence: true
+  validates :contractor, presence: true
+  validates :parking_space, presence: true
+  validates :parking_manager, presence: true
 
   belongs_to :contractor
   belongs_to :parking_space
