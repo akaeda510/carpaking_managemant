ac45aaa5ac0d771ab6e443875c9f52659b07b955
akaeda510 <moontype510@i.softbank.jp>
2025-12-14 17:25:56 +0900

    update:model_contractor_validates

--- app/models/contractor.rb
+++ app/models/contractor.rb
@@ -12,6 +12,7 @@ class Contractor < ApplicationRecord
   validates :phone_number, presence: true, length: { is: 11 }, numericality: { only_integer: true }, uniqueness: true
   validates :contact_number, length: { minimum: 10, maximum: 11 }, numericality: { only_integer: true }, allow_nil: true, allow_blank: true
   validates :notes, length: { maximum: 150 }
+  validates :parking_manager, presence: true
 
   has_many :contract_parking_spaces, dependent: :destroy
   has_many :active_contract_parking_spaces, -> {
