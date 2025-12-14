e899971a00040530d2860e3e4efc565183e1d612
akaeda510 <moontype510@i.softbank.jp>
2025-12-14 15:16:12 +0900

    add:test_contract_parking_space

---
+++ test/controllers/contract_parking_spaces_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class ContractParkingSpacesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/models/contract_parking_space_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class ContractParkingSpaceTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
