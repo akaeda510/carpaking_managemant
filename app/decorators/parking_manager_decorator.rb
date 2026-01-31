8de935897de12a0147a52aa7942a7a8f921a3da2
akaeda510 <moontype510@i.softbank.jp>
2026-01-31 12:49:21 +0900

    update:decorate_parking_manager

--- app/decorators/parking_manager_decorator.rb
+++ app/decorators/parking_manager_decorator.rb
@@ -1,15 +1,16 @@
 class ParkingManagerDecorator < Draper::Decorator
   delegate_all
 
-  # Define presentation-specific methods here. Helpers are accessed through
-  # `helpers` (aka `h`). You can override attributes, for example:
-  #
   def formatted_created_at
-    object.created_at.strftime("%Y年%m月%d日")
+    object.created_at.strftime("%Y年/%m月/%d日")
   end
 
   def full_name
     "#{object.first_name} #{object.last_name}"
+  end
+
+  def address
+    "#{object.prefecture} #{object.city} #{object.street_address}"
   end
 
   def phone_number
@@ -17,10 +18,6 @@ class ParkingManagerDecorator < Draper::Decorator
   end
 
   def contact_number
-    object.contact_number.to_s.gsub(/(\d{2,5})(\d{1,4})(\d{4})/, '\1-\2-\3')
-  end
-
-  def address
-    "#{object.prefecture} #{object.city} #{object.street_address}"
+    object.contact_number.to_s.gsub(/(\d{2,4})(\d{2,4})(\d{4})/, '\1-\2-\3')
   end
 end
