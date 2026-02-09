375cf9bd66815dc11474918792d38058ac166b7a
akaeda510 <moontype510@i.softbank.jp>
2026-02-10 00:02:15 +0900

    add:駐車スペースオプションテーブルを作成

--- db/migrate/20260209142419_create_parking_space_options.rb
+++ db/migrate/20260209142419_create_parking_space_options.rb
@@ -1,7 +1,13 @@
 class CreateParkingSpaceOptions < ActiveRecord::Migration[8.1]
   def change
     create_table :parking_space_options do |t|
+      t.string :name,               null: false
+
+      t.references :parking_space,     null: false, foreign_key: true
+
       t.timestamps
+
+      t.index :name
     end
   end
 end
