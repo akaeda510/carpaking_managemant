6700b8a639b7376e05c2b916414e38f4ad742235
akaeda510 <moontype510@i.softbank.jp>
2026-02-12 00:03:18 +0900

    update:マイグレーションファイルのカラムを変更

--- db/migrate/20260209142419_create_parking_space_options.rb
+++ db/migrate/20260209142419_create_parking_space_options.rb
@@ -3,10 +3,7 @@ class CreateParkingSpaceOptions < ActiveRecord::Migration[8.1]
     create_table :parking_space_options do |t|
       t.string :name,               null: false
 
-      t.references :parking_space,     null: false, foreign_key: true
-
       t.timestamps
-
       t.index :name
     end
   end
