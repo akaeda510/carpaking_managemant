25df01de4efe9e86d079a64f7a85c06579237b55
akaeda510 <moontype510@i.softbank.jp>
2026-02-26 18:17:08 +0900

    fix:parking_manager_logout_pathの見直し

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -44,6 +44,8 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource_or_scope)
     if resource_or_scope == :admin
       new_admin_session_path
+    elsif resource_or_scope == :parking_manager
+      new_parking_manager_session_path
     else
       root_path
     end
