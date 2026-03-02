6e0ae445f77e35d4ffbd8e80f6e0b6db7617d5d2
akaeda510 <moontype510@i.softbank.jp>
2026-03-02 14:44:20 +0900

    lint修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-
   namespace :admin do
-    get 'login', to: 'sessions#new'
-    post 'login', to: 'sessions#create'
-    delete 'logout', to: 'sessions#destroy'
+    get "login", to: "sessions#new"
+    post "login", to: "sessions#create"
+    delete "logout", to: "sessions#destroy"
 
     root to: "dashboards#show"
     resources :parking_managers, only: %i[ index show ]
--- db/migrate/20260301105333_convert_admin_to_secure_password.rb
+++ db/migrate/20260301105333_convert_admin_to_secure_password.rb
@@ -4,7 +4,7 @@ class ConvertAdminToSecurePassword < ActiveRecord::Migration[8.1]
 
     remove_index :admins, :reset_password_token if index_exists?(:admins, :reset_password_token)
 
-    remove_column :admins, :reset_password_token, :string 
+    remove_column :admins, :reset_password_token, :string
     remove_column :admins, :reset_password_sent_at, :datetime
     remove_column :admins, :remember_created_at, :datetime
   end
