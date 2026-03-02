2582731fd3395eb5175644badd219793072d9020
akaeda510 <moontype510@i.softbank.jp>
2026-03-02 14:36:09 +0900

    udpate:ハッシュ化したパスワードの関連付けを定義

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,8 +1,5 @@
 class Admin < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  has_secure_password
 
   validates :first_name, presence: true, length: { maximum: 20 }
   validates :last_name, presence: true, length: { maximum: 20 }
