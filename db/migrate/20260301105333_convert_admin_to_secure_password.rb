12082e9cf742138440dabf182667abf12ecc42b0
akaeda510 <moontype510@i.softbank.jp>
2026-03-02 14:33:48 +0900

    add:管理者アカウントをDeviseから独自のアカウント設計に変更のためテーブルを再設計

---
+++ db/migrate/20260301105333_convert_admin_to_secure_password.rb
@@ -0,0 +1,11 @@
+class ConvertAdminToSecurePassword < ActiveRecord::Migration[8.1]
+  def change
+    rename_column :admins, :encrypted_password, :password_digest
+
+    remove_index :admins, :reset_password_token if index_exists?(:admins, :reset_password_token)
+
+    remove_column :admins, :reset_password_token, :string 
+    remove_column :admins, :reset_password_sent_at, :datetime
+    remove_column :admins, :remember_created_at, :datetime
+  end
+end
