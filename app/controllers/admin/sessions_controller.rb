34266f2c4ba7a903c9b0c3b5074789c9143fcd35
akaeda510 <moontype510@i.softbank.jp>
2026-03-02 19:43:29 +0900

    修正

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,6 +1,5 @@
 class Admin::SessionsController < Admin::BaseController
   skip_before_action :authenticate_admin!, only: %i[new create], raise: false
-  skip_authorization_only :new, :create, :destroy
 
   def new; end
 
