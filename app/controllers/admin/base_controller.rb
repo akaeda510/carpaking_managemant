8aba4b0a5ba97c6046c5449d9f472b591b287273
akaeda510 <moontype510@i.softbank.jp>
2026-03-02 14:39:06 +0900

    update:ログイン機能の定義

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,4 +1,17 @@
 class Admin::BaseController < ApplicationController
   before_action :authenticate_admin!
   layout "admin"
+
+  def current_admin
+    @current_admin ||= ::Admin.find_by(id: session[:admin_id]) if session[:admin_id]
+  end
+  helper_method :current_admin
+
+  private
+
+  def authenticate_admin!
+    if current_admin.nil?
+      redirect_to admin_login_path, alert: "管理者のログインが必要です"
+    end
+  end
 end
