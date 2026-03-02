e460e47497a3c9330cb13f005eb3f7a43dfaf6f6
akaeda510 <moontype510@i.softbank.jp>
2026-03-02 18:53:51 +0900

    update:admin_dashboardのルーティングを明確に定義

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     post "login", to: "sessions#create"
     delete "logout", to: "sessions#destroy"
 
-    root to: "dashboards#index"
+    root to: "admin/dashboards#index"
     resources :parking_managers, only: %i[ index show ]
     resources :contractors, only: %i[ index show ] do
       member do
