3344284511d37bae028ca3db7927a864ebb21e2e
akaeda510 <moontype510@i.softbank.jp>
2026-03-05 17:03:25 +0900

    update:同じ名前の時エラーメッセージを表示

--- app/models/parking_area.rb
+++ app/models/parking_area.rb
@@ -1,9 +1,9 @@
 class ParkingArea < ApplicationRecord
-  validates :name, presence: true, length: { maximum: 20 }
+  validates :name, presence: true, length: { maximum: 20 }, uniqueness: { scope: :parking_lot_id, message: "はすでに駐車場内で使用されています" }
   validates :default_price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :parking_lot, presence: true
 
-  enum :category { asphalt: 0, gravel: 1, garage: 2 }
+  enum :category, { asphalt: 0, gravel: 1, garage: 2, others: 99 }, prefix: true
 
   belongs_to :parking_lot
 
