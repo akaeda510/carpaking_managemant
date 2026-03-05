254e067074cda91bb2c68e194060ba1cd5f1ae3b
akaeda510 <moontype510@i.softbank.jp>
2026-03-05 17:42:35 +0900

    update:parking_area_edit_updateのコントロールを定義

--- app/controllers/parking_areas_controller.rb
+++ app/controllers/parking_areas_controller.rb
@@ -1,6 +1,7 @@
 class ParkingAreasController < ApplicationController
   before_action :authenticate_parking_manager!
   before_action :set_parking_lot
+  before_action :set_parking_area, only: %i[ edit update ]
 
   def new
     @parking_area = @parking_lot.parking_areas.build(
@@ -13,7 +14,7 @@ class ParkingAreasController < ApplicationController
   def create
     @parking_area = @parking_lot.parking_areas.build(parking_area_params)
     if @parking_area.save
-      redirect_to parking_lot_parking_areas_path(@parking_lot), seccess: "エリアを作成しました。", status: :see_other
+      redirect_to parking_lot_parking_areas_path(@parking_lot), success: "エリアを作成しました。", status: :see_other
     else
       render :new, status: :unprocessable_entity
     end
@@ -23,13 +24,27 @@ class ParkingAreasController < ApplicationController
     @parking_areas = @parking_lot.parking_areas.order(:created_at)
   end
 
+  def edit; end
+
+  def update
+    if @parking_area.update(parking_area_params)
+      redirect_to parking_lot_parking_areas_path(@parking_lot), success: "エリアが更新されました。", status: :see_other
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def parking_area_params
-    params.require(:parking_area).permit(:name, :default_price)
+    params.require(:parking_area).permit(:name, :default_price, :category)
   end
 
   def set_parking_lot
     @parking_lot = current_parking_manager.parking_lots.find(params[:parking_lot_id])
+  end
+
+  def set_parking_area
+    @parking_area = @parking_lot.parking_areas.find(params[:id])
   end
 end
