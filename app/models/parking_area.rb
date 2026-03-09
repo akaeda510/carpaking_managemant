5e4364e65931f47968b8dc78d72d0c53147f7976
akaeda510 <moontype510@i.softbank.jp>
2026-03-09 11:56:13 +0900

    update:parking_areaに新たなにカラムを追加によるバリデーションを定義

--- app/models/parking_area.rb
+++ app/models/parking_area.rb
@@ -1,6 +1,7 @@
 class ParkingArea < ApplicationRecord
   validates :name, presence: true, length: { maximum: 20 }, uniqueness: { scope: :parking_lot_id, message: "はすでに駐車場内で使用されています" }
   validates :default_price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :description, length: { maximum: 150 }
   validates :parking_lot, presence: true
 
   enum :category, { asphalt: 0, gravel: 1, garage: 2, others: 99 }, prefix: true
