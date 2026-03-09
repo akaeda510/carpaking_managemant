4639a01fb459f9066c46e81bb3f061bff4b2f8e2
akaeda510 <moontype510@i.softbank.jp>
2026-03-09 10:32:29 +0900

    update:admin_parking_space_indexのルーティングを定義

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,8 @@ Rails.application.routes.draw do
     resources :parking_managers, only: %i[ index show ] do
       resources :contractors, only: %i[ index show ], shallow: true
     end
-    resources :parking_lots, only: %i[ index show ], shallow: true 
+    resources :parking_lots, only: %i[ index show ], shallow: true do
+      resources :parking_space, only: %i[ index ], shallow: true
     end
   end
 
