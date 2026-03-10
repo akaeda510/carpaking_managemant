2b835341375fbc71a9225dae071b7d019bd7ea09
akaeda510 <moontype510@i.softbank.jp>
2026-03-10 11:37:37 +0900

    update:admin_parking_space_showのルーティングを定義

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,9 @@ Rails.application.routes.draw do
       resources :contractors, only: %i[ index show ], shallow: true
     end
     resources :parking_lots, only: %i[ index show ], shallow: true do
-      resources :parking_areas, only: %i[ show ], shallow: true
+      resources :parking_areas, only: %i[ show ], shallow: true do
+        resources :parking_spaces, only: %i[ show ], shallow: true
+      end
     end
   end
 
