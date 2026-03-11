80f15051210955123528e6c3d690a45f82359db3
akaeda510 <moontype510@i.softbank.jp>
2026-03-11 17:42:49 +0900

    update:showコントロールにガレージ、オプション、駐車場契約情報を定義

--- app/controllers/admin/parking_spaces_controller.rb
+++ app/controllers/admin/parking_spaces_controller.rb
@@ -5,10 +5,14 @@ class Admin::ParkingSpacesController < Admin::BaseController
 
   def show
     @current_contract = @parking_space.current_contract
+    @garage_deteil = @parking_space.garage_detail
+    @parking_space_options = @parking_space.parking_space_options
+    @current_contract = @parking_space.current_contract
+    @contract_histories = @parking_space.contract_parking_spaces.order(start_date: :desc)
   end
 
   private
-  
+
   def set_parking_lot
     @parking_lot = @parking_area.parking_lot
   end
@@ -18,6 +22,6 @@ class Admin::ParkingSpacesController < Admin::BaseController
   end
 
   def set_parking_space
-    @parking_space = current_admin.parking_spaces.find(params[:id]
+    @parking_space = current_admin.parking_spaces.find(params[:id])
   end
 end
