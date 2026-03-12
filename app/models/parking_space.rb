14a04643b0c356006c3e02b31c278eca3f4c9bc6
akaeda510 <moontype510@i.softbank.jp>
2026-03-12 13:16:58 +0900

    update:parking_spaceの検索機能に契約者検索を追加

--- app/models/parking_space.rb
+++ app/models/parking_space.rb
@@ -2,10 +2,16 @@ class ParkingSpace < ApplicationRecord
 
   include PgSearch::Model
 
-  pg_search_scope :seach_full_text,
-    against: { name: 'A', description: 'B' },
+  pg_search_scope :search_full_text,
+    against: { 
+      name: 'A',
+      description: 'B'
+    },
+    associated_against: {
+      contractors: [:first_name, :last_name]
+    },
     using: {
-      testarch: { 
+      tsearch: { 
         prefix: true,
         dictionary: "simple",
         any_word: true
@@ -29,8 +35,7 @@ class ParkingSpace < ApplicationRecord
   # 駐車スペースの契約状態スペースを消そうとする時に、契約データがあった場合、エラーで処理を阻止
   has_many :contract_parking_spaces, dependent: :restrict_with_exception
   has_many :active_contractor_parking_spaces, -> { where("end_date >= ?", Date.current) }, class_name: "ContractParkingSpace"
-  has_many :contractor, through: :contractor_parking_spaces
-
+  has_many :contractors, through: :contract_parking_spaces
   has_many :parking_space_option_assignments, dependent: :destroy
   has_many :parking_space_options, through: :parking_space_option_assignments
 
