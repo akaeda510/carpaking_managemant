82dfe3811666e6b0273368b65edc5b64e240f853
akaeda510 <moontype510@i.softbank.jp>
2026-03-16 15:44:05 +0900

    update:メール設定を削除

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,16 +64,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via bin/rails credentials:edit.
-  config.action_mailer.smtp_settings = {
-    address: '153.120.129.231',
-    port: 2525,
-    domain: 'carpaking-managemant.onrender.com',
-    user_name: 'be68659af',
-    password: ENV['BLASTENGINE_API_KEY'],
-    authentication: :plain,
-    enable_starttls_auto: true
-  }
-  
+ 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
