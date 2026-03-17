3477342ed6b8e2dab694af54964680eb3f6970e6
akaeda510 <moontype510@i.softbank.jp>
2026-03-17 21:02:40 +0900

    update:変数の修正

--- app/mailers/parking_managers/login_mailer.rb
+++ app/mailers/parking_managers/login_mailer.rb
@@ -2,7 +2,7 @@ class ParkingManagers::LoginMailer < ApplicationMailer
   def login_notification(parking_manager, ip_address)
     @parking_manager = parking_manager.decorate
 
-    transaction = BLASTENGINE::Transaction.new(BLASTENGINE)
+    transaction = BLASTENGINE.transaction
     transaction.from = "info@tukigime-parking.com"
     transaction.to = @parking_manager.email
     transaction.subject = "【重要】ログイン通知"
