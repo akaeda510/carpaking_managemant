c7687598fef8a4febc3a6ba42d03a742183a2554
akaeda510 <moontype510@i.softbank.jp>
2026-03-19 09:27:17 +0900

    update:コードの書き方を変更

--- app/mailers/parking_managers/login_mailer.rb
+++ app/mailers/parking_managers/login_mailer.rb
@@ -3,12 +3,14 @@ class ParkingManagers::LoginMailer < ApplicationMailer
     @parking_manager = parking_manager.decorate
 
     transaction = Blastengine::Transaction.new
-    transaction.from(email: "info@tukigime-parking.com")
-    transaction.to(address: @parking_manager.email)
-    transaction.subject("【重要】ログイン通知")
+    transaction.from email: "info@tukigime-parking.com", name: "管理者"
+    transaction.to = @parking_manager.email
+    transaction.subject = "【重要】ログイン通知"
 
-    transaction.text_part(render_to_string(template: "notification_mailer/login_notification"))
+    transaction.text_part = render_to_string(template: "notification_mailer/login_notification")
 
-    transaction.deliver
+    delibery_id = transaction.send
+
+    Rails.logger.info "--- Blastengine Email Sent! ID: #{delivery_id} ---"
   end
 end
