cad82b10c99556999f1f2c76800eb50af3eea997
akaeda510 <moontype510@i.softbank.jp>
2026-03-19 20:11:36 +0900

    update:継承をBlastBaseMailerに変更

--- app/mailers/parking_managers/login_mailer.rb
+++ app/mailers/parking_managers/login_mailer.rb
@@ -1,19 +1,22 @@
-class ParkingManagers::LoginMailer < ApplicationMailer
+class ParkingManagers::LoginMailer < BlastengineBaseMailer
   def login_notification(parking_manager)
     @parking_manager = parking_manager.decorate
     @login_time = Time.current
 
-    deliver_via_api(
-      to = @parking_manager.email,
-      subject = "【重要】ログイン通知"
-    )
-
-    html_part = render_to_string(
+    html_content = render_to_string(
       template: "parking_managers/mailer/login_notification",
       formats: [ :html ]
     )
 
-    text_part = "駐車場管理システムにログインがありました。日時: #{@login_time}"
+    text_content = "駐車場管理システムにログインがありました。日時: #{@login_time}"
+
+    deliver_via_api(
+      from_type: :system,
+      to:  @parking_manager.email,
+      subject: "【重要】ログイン通知",
+      html_part: html_content,
+      text_part: text_content
+    )
 
   end
 end
