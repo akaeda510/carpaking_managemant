36e905303ac6e036ae832ee65e27e6097930cc7b
akaeda510 <moontype510@i.softbank.jp>
2026-03-19 12:20:39 +0900

    update:blastengineのトークン設定を修正

--- config/initializers/blastengine.rb
+++ config/initializers/blastengine.rb
@@ -1,1 +1,6 @@
-BLASTENGINE = Blastengine::Client.new(ENV["BLASTENGINE_API_KEY"], "be68659af")
+require "blastengine"
+
+Blastengine.initialize(
+  api_key: ENV["BLASTENGINE_API_KEY"],
+  user_name: ENV["BLASTENGINE_USER_ID"]
+)
