3caa1ac3a275862788198576c75e3f57437021ae
akaeda510 <moontype510@i.softbank.jp>
2026-03-19 20:11:59 +0900

    update:コードの書き方を変更

--- app/mailers/parking_managers/device_mailer.rb
+++ app/mailers/parking_managers/device_mailer.rb
@@ -3,17 +3,16 @@ class ParkingManagers::DeviceMailer < BlastengineBaseMailer
 
   def warning_new_device_login(parking_manager, device)
     @parking_manager = parking_manager.decorate
-    @device = device
-    @verify_url = verify_device_url(@device.device_token)
+    @device = device.decorate
+    @verify_url = verify_devices_url(device_token: @device.device_token)
+
+    login_time = @device.login_timestamp
 
     html_content = render_to_string(
       template: "parking_managers/mailer/warning_new_device_login",
       formats: [ :html ]
     )
 
-    deliver_via_api(
-      to = @parking_manager.email,
-      subject = "【重要】新しい端末からのログインを検知しました",
       text_content = <<~TEXT
       #{@parking_manager.full_name} 様
 
@@ -24,5 +23,14 @@ class ParkingManagers::DeviceMailer < BlastengineBaseMailer
       心当たりがある場合は、以下のURLから端末を登録してください。
       #{@verify_url}
       TEXT
+
+
+    deliver_via_api(
+      from_type: :system,
+      to: @parking_manager.email,
+      subject: "【重要】新しい端末からのログインを検知しました",
+      html_part: html_content,
+      text_part: text_content
+    )
   end
 end
