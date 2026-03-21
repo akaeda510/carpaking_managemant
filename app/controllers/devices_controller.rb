c5eba3b0c8111687ff9f26d2fdea15dee19f9f06
akaeda510 <moontype510@i.softbank.jp>
2026-03-21 13:39:42 +0900

    update:端末未登録時の処理フローを作成

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -1,11 +1,18 @@
 class DevicesController < ApplicationController
+  skip_before_action :authenticate_parking_manager!, only: [:verify]
+
   def verify
     @device = Device.find_by!(device_token: params[:id])
 
+    if @device.expires_at < Time.current
+      redirect_to new_parking_manager_session_path, alert: "登録期限が切れています。再度ログインをしてください。"
+      return
+    end
+
     if @device.update(is_verified: true)
-      redirect_to root_path, success: "端末「#{@device.name}」を承認しました。"
+      @redirect_path = parking_manager_signed_in? ? root_path: new_parking_manager_session_path
     else
-      redirect_to root_path, alert: "承認に失敗しました。"
+      redirect_to new_parking_manager_session_path, alert: "登録に失敗しました。"
     end
   end
 end
