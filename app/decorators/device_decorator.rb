fe615ac82867eb89bec2e643a4b4d7b0ac22f0a9
akaeda510 <moontype510@i.softbank.jp>
2026-03-23 15:12:39 +0900

    update:friendly_nameを定義

--- app/decorators/device_decorator.rb
+++ app/decorators/device_decorator.rb
@@ -4,4 +4,13 @@ class DeviceDecorator < Draper::Decorator
   def login_timestamp
     object.last_login_at.strftime("%Y-%m-%d %H:%M:%S")
   end
+
+  def friendly_name(remote_ip)
+    location = Geocoder.search(remote_ip).first
+    city_info = location&.city.present? ? "#{location.city}付近の" : "不明な場所の"
+
+    device_label = Device.set_name_by_user_agent(device.user_agent, nil)
+
+    "#{city_info}#{device_label}"
+  end
 end
