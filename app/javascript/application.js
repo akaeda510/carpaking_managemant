0a83c030b09d91d5ad298ab6dde9a3bd98b77856
akaeda510 <moontype510@i.softbank.jp>
2026-03-25 18:47:56 +0900

    update:エラーの後でも機能するように修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,7 +3,10 @@ import "./controllers"
 import { PasswordToggle } from "./utils/password_toggle"
 import { PasswordValidator } from "./utils/password_validator"
 
-document.addEventListener("turbo:load", () => {
+const initUtils = () => {
 	PasswordToggle.init();
 	PasswordValidator.init();
-});
+};
+
+document.addEventListener("turbo:load", initUtils);
+document.addEventListener('turbo:render', initUtils);
