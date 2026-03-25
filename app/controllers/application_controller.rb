2eec40af234e6a8f3da0fdb1a1110fa9a93b9c74
akaeda510 <moontype510@i.softbank.jp>
2026-03-25 22:14:52 +0900

    update:端末登録確認ページのサイドバーが表示されていたのを修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ require "pundit"
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  before_action :confirm_device_verified!, unless: :devise_controller?
   before_action :configure_permitted_parameters, if: :devise_controller?
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
   layout :layout_by_resource
