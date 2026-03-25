83821c51060a249341bd50ee35df76918f0b7700
akaeda510 <moontype510@i.softbank.jp>
2026-03-25 16:06:31 +0900

    update:目アイコンとパスワードリアルバリデーションを有効化

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,9 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
+import { PasswordToggle } from "./utils/password_toggle"
+import { PasswordValidator } from "./utils/password_validator"
+
+document.addEventListener("turbo:load", () => {
+	PasswordToggle.init();
+	PasswordValidator.init();
+});
