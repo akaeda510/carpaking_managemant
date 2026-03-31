084563faac5f83526abe5d3b2e6ad9a767d77055
akaeda510 <moontype510@i.softbank.jp>
2026-03-31 15:51:39 +0900

    update:ページ表示設定を変更

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,4 @@
 class StaticPagesController < ApplicationController
-  skip_before_action :authenticate_parking_manager!
-  skip_before_action :confirm_device_verified!
 
   def privacy; end
-
-  def settings; end
 end
