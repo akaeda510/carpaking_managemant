6b4583b23c9df139ad87965d6e995aebab9b5895
akaeda510 <moontype510@i.softbank.jp>
2026-04-01 18:26:57 +0900

    add:設定ページのコントロールを作成

--- app/controllers/parking_managers/settings_controller.rb
+++ app/controllers/parking_managers/settings_controller.rb
@@ -1,5 +1,14 @@
 class ParkingManagers::SettingsController < ApplicationController
+  before_action :authenticate_parking_manager!
+  before_action :confirm_device_verified!
+
   def index
     authorize :parking_manager, :index?
+    render :index, locals: { partial_name: "top_setting" }
+  end
+
+  def account
+    @parking_manager = current_parking_manager
+    render :index, locals: { partial_name: "account_setting" }
   end
 end
