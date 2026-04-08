eaeb28f0e6dff339e3ea7c74df5b24dd3021a014
akaeda510 <moontype510@i.softbank.jp>
2026-04-08 15:47:15 +0900

    update:パスワードリセット時のセッション状態によっての分岐を修正

--- app/controllers/parking_managers/passwords_controller.rb
+++ app/controllers/parking_managers/passwords_controller.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class ParkingManagers::PasswordsController < Devise::PasswordsController
-  prepend_before_action :authenticate_parking_manager!, only: %i[ new create ], if: -> { parking_manager_signed_in? }
-  skip_before_action :require_no_authentication, only: %i[ new create edit ]
+  prepend_before_action :authenticate_parking_manager!, only: %i[ new create edit update ], if: -> { parking_manager_signed_in? }
+  skip_before_action :require_no_authentication, only: %i[ new create edit update ]
   append_before_action :assert_reset_token_passed, only: :edit
   # GET /resource/password/new
   # def new
@@ -16,12 +16,7 @@ class ParkingManagers::PasswordsController < Devise::PasswordsController
 
     if successfully_sent?(resource)
       set_flash_message!(:notice, :send_instructions)
-
-      if parking_manager_signed_in?
-        respond_with resource, location: after_sign_in_path_for(resource)
-      else
-        respond_with resource, location: new_parking_manager_session_path
-      end
+      respond_with resource, location: auth_dependent_redirect_path(resource)
     else
       respond_with(resource)
     end
@@ -31,7 +26,7 @@ class ParkingManagers::PasswordsController < Devise::PasswordsController
     # パスワードリセット後、トークンを失効し、独自のパスを指定
     resource = resource_class.with_reset_password_token(params[:reset_password_token])
 
-    unless resource&& resource.reset_password_period_balid?
+    unless resource&& resource.reset_password_period_valid?
       set_flash_message! :alert, :no_token_found
 
       if parking_manager_signed_in?
@@ -47,29 +42,36 @@ class ParkingManagers::PasswordsController < Devise::PasswordsController
 
   # PUT /resource/password
   def update
-    super do |resource|
-      if resource.errors.empty?
-        register_and_verify_current_device(resource)
+    # 処理初めのログイン状態を記録
+    signed_in_before_update = parking_manager_signed_in?
 
+    self.resource = resource_class.reset_password_by_token(resource_params)
+    yield resource if block_given?
 
-        if parking_manager_signed_in?
-          set_flash_message! :notice, :updated
-          bypass_sign_in(resource)
-          respond_with resource, location: after_sign_in_path_for(resource)
-        else
-          set_flash_message! :notice, :updated_not_active
-          respond_with resource, location: new_parking_manager_session_path
-        end
-        return
+    if resource.errors.empty?
+      register_and_verify_current_device(resource)
+      complete_auth_flow(resource, confirmation_token: params[:reset_password_token])
+
+      # 未ログイン状態の場合、ログインを解除
+      unless signed_in_before_update
+        sign_out(resource_name)
       end
+
+      set_flash_message!(:notice, :updated)
+      # パスワードへんこうで切れたセッションを上書きして保持
+      respond_with resource, location: auth_dependent_redirect_path(resource)
+    else
+      persist_session_on_failure(resource)
+      # 最低文字数を表示
+      set_minimum_password_length
+      respond_with resource
     end
   end
 
   protected
 
   def register_and_verify_current_device(resource)
-    device = Devices.find_or_initialize_by(resource, cookies[:device_token])
-
+    device = Device.find_or_initialize_by(parking_manager: resource, device_token: cookies[:device_token])
     # デバイスの検証済みとして保存
     if device.verify_with_agent!(request.user_agent)
       cookies[:device_token] = {
@@ -77,10 +79,13 @@ class ParkingManagers::PasswordsController < Devise::PasswordsController
         expires: 1.month.from_now,
         httponly: true
       }
-
-      session.delete(:need_verification)
-      session.delete(:pending_device_id)
+      clear_auth_session_data
     end
+  end
+
+  def complete_auth_flow(resource, confirmation_token: nil)
+    clear_auth_session_data(confirmation_token: nil)
+    persist_session_on_failure(resource)
   end
 
   # def after_resetting_password_path_for(resource)
