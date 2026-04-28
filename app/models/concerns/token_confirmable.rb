a7776c13da77e4bcc4c29b7456e5d1d16783ec42
akaeda510 <moontype510@i.softbank.jp>
2026-04-28 15:58:36 +0900

    update:active?メソッドの例外処理を追加

--- app/models/concerns/token_confirmable.rb
+++ app/models/concerns/token_confirmable.rb
@@ -7,10 +7,17 @@ module TokenConfirmable
     before_validation :generate_token, on: :create
   end
 
+  def active?(expires_in: 30.minutes)
+    # 送信時刻が空または、すでに登録済みなら無効
+    return false if confirmation_sent_at.nil? || confirmed_at.present?
+    (confirmed_sent_at + expires_in) > Time.current
+  end
+
   def valid_token?(expires_in: 30.minutes)
     confirmed_at.nil? && confirmation_sent_at > expires_in.ago
   end
 
+  # トークンの更新
   def confirm!
     update!(confirmed_at: Time.current)
   end
