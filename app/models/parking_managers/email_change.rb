cdfff24e8cd635f9472192da30b48a4c1f0aca5b
akaeda510 <moontype510@i.softbank.jp>
2026-04-28 16:33:18 +0900

    update:新たに同じアドレスを更新できないバリデーションエラーを追加

--- app/models/parking_managers/email_change.rb
+++ app/models/parking_managers/email_change.rb
@@ -4,5 +4,13 @@ class ParkingManagers::EmailChange < ApplicationRecord
 
   belongs_to :parking_manager
 
-  validates :new_email, presence: true, format: { with: Devise.email_regexp }
+  validates :new_email, presence: true, format: { with: Devise.email_regexp }, uniqueness: { scope: parking_manager_id}
+  validate :email_must_be_different
+  private
+
+  def email_must_be_different
+    if new_email == parking_manager.email
+      errors.add(:new_email, "現在のメールアドレスと同じものは登録できません")
+    end
+  end
 end
