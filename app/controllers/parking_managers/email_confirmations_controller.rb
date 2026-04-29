bf0732491203a02f1b515ce3d3097505c6d8f084
akaeda510 <moontype510@i.softbank.jp>
2026-04-29 13:15:40 +0900

    update:email_confirmation_paramsを作成

--- app/controllers/parking_managers/email_confirmations_controller.rb
+++ app/controllers/parking_managers/email_confirmations_controller.rb
@@ -4,18 +4,24 @@ class ParkingManagers::EmailConfirmationsController < ApplicationController
   end
 
   def create
-    email = params[:email_confirmation][:email]
+    email = email_confirmation_params[:email]
 
-    if ParkingManager.exists?(email: email)
+    if ParkingManager.exists?(email_confirmation_params)
       Rails.logger.info "⚠️ [SKIP] Email already exists: #{email}はすでに登録済みです"
       redirect_to new_parking_manager_session_path, success: "登録用メールが送信されました。"
     else
       Rails.logger.info "📧 [SEND] Preparing to send email to: #{email}の確認レコードを作成します"
 
-      confirmation = EmailConfirmation.create!(email: email)
+      confirmation = EmailConfirmation.create!(email_confirmation_params)
       ParkingManagers::RegistrationMailer.confirmation_link(confirmation).deliver_later
 
       redirect_to new_parking_manager_session_path, success: "登録用メールが送信されました。"
     end
+  end
+
+  private
+
+  def email_confirmation_params
+    params.require(:email_confirmation).permit(:email)
   end
 end
