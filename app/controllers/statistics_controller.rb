0cf45f1dd8b5784a7e25ea9a762dc7f0b92f364f
akaeda510 <moontype510@i.softbank.jp>
2026-06-05 17:23:36 +0900

    add:ダッシュボードの駐車稼働率統計機能のコントロールを作成

---
+++ app/controllers/statistics_controller.rb
@@ -0,0 +1,12 @@
+class StatisticsController < ApplicationController
+  before_action :authenticate_parking_manager!
+  before_action :enforce_device_verification
+
+  def index
+    @parking_manager = current_parking_manager
+
+    stats = ParkingStatisticsService.new(@parking_manager, nil)
+    
+    @status_counts = stats.status_counts
+    @monthly_sales = stats.monthly_sales
+end
