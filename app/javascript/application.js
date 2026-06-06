72726111f42399758981bf299afdb3082ce51836
akaeda510 <moontype510@i.softbank.jp>
2026-06-06 15:24:42 +0900

    update:統計グラフの実装を登録

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,11 +3,17 @@ import "./controllers"
 import "chartkick/chart.js"
 import { PasswordToggle } from "./utils/password_toggle"
 import { PasswordValidator } from "./utils/password_validator"
+import { occupancyChartOptions } from "./charts/occupancy_chart"
 
 const initUtils = () => {
 	PasswordToggle.init();
 	PasswordValidator.init();
 };
 
-document.addEventListener("turbo:load", initUtils);
-document.addEventListener('turbo:render', initUtils);
+document.addEventListener("turbo:load", () => { 
+	initUtils()
+	const chart = Chartkick.charts["occupancy-chart"]
+	if (chart) {
+		chart.updateOptions(occupancyChartOptions) } 
+});
+document.addEventListener("turbo:render", initUtils);
