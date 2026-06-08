5e8e1490fbaaf43060098f225dd0aa085ce77205
akaeda510 <moontype510@i.softbank.jp>
2026-06-08 17:35:34 +0900

    lint修正

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -6,7 +6,7 @@ class StatisticsController < ApplicationController
     @parking_manager = current_parking_manager
 
     stats = ParkingStatisticsService.new(@parking_manager, nil)
-    
+
     @status_counts = stats.status_counts
     @monthly_sales = stats.monthly_sales
   end
