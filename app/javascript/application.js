a45850348191b4f15a1c7630487330824ef72573
akaeda510 <moontype510@i.softbank.jp>
2026-06-08 16:29:49 +0900

    update:グラフ表示のタイミングを修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,7 +3,6 @@ import "./controllers"
 import "chartkick/chart.js"
 import { PasswordToggle } from "./utils/password_toggle"
 import { PasswordValidator } from "./utils/password_validator"
-import { occupancyChartOptions } from "./charts/occupancy_chart"
 
 const initUtils = () => {
 	PasswordToggle.init();
@@ -12,8 +11,5 @@ const initUtils = () => {
 
 document.addEventListener("turbo:load", () => { 
 	initUtils()
-	const chart = Chartkick.charts["occupancy-chart"]
-	if (chart) {
-		chart.updateOptions(occupancyChartOptions) } 
 });
 document.addEventListener("turbo:render", initUtils);
