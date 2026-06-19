5b6697f88caf13aef6455db95fa3f6f6e5c9ce0e
akaeda510 <moontype510@i.softbank.jp>
2026-06-19 17:09:59 +0900

    update:parking_managerのテストデータを作成

--- test/factories/parking_managers.rb
+++ test/factories/parking_managers.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :parking_manager do
     
+    first_name { Faker::JapaneseName.first_name }
+    last_name { Faker::JapaneseName.last_name }
+    email { Faker::Internet.email }
+    password { Faker::Internet.password }
+    prefecture { Faker::Address.prefecture }
+    city { Faker::Address.city }
+    street_address { Faker::Address.street_address }
+    phone_number { Faker::Number.number(digits: 11).to_s }
+    contact_number { Faker::Number.number(digits: 10).to_s }
+    terms_of_service { true }
   end
 end
