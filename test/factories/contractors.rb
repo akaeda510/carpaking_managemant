0542e969be59c6cf23bfc008c739eabddab22ca7
akaeda510 <moontype510@i.softbank.jp>
2026-06-22 15:56:33 +0900

    add:contractorのfactorybotを作成

---
+++ test/factories/contractors.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :contractor do
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name } 
+    prefecture { I18n.t("prefectures").values.sample }
+    city { Faker::Address.city }
+    street_address { Faker::Address.street_address }
+    phone_number { Faker::Number.number(digits: 11).to_s }
+    contact_number { Faker::Number.number(digits: 10).to_s }
+  end
+end
