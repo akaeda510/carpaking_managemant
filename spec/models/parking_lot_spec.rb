02736796611e89fb9d3f39008a4961146c8245d3
akaeda510 <moontype510@i.softbank.jp>
2026-06-23 14:22:48 +0900

    feat:rspec_parking_lotのstreet_addressが空欄だった場合のエラーテストを実装

--- spec/models/parking_lot_spec.rb
+++ spec/models/parking_lot_spec.rb
@@ -46,6 +46,8 @@ RSpec.describe ParkingLot, type: :model do
       end
 
       it 'street_addressが空欄の場合' do
+        parking_lot.street_address = nil
+        expect(parking_lot).to be_invalid
       end
 
 
