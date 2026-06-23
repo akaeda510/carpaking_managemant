b5fd637244f12700e6619ce5e8ef3bbcb2bb92ff
akaeda510 <moontype510@i.softbank.jp>
2026-06-23 13:51:47 +0900

    update:rspec_parking_lot_createの条件を定義

--- spec/models/parking_lot_spec.rb
+++ spec/models/parking_lot_spec.rb
@@ -1,5 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe ParkingLot, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'create' do
+    let(:parking_manager) { FactoryBot.create(:parking_manager) }
+    let(:parking_lot) { FactoryBot.build(:parking_lot) }
+    
+    # 成功パターン
+    context 'バリデーション' do 
+      it '設定した全てのバリデーションが機能しているか' do
+      end
+    end
+
+    # 失敗パターン
+    context 'バリデーション' do
+      it 'nameが41文字以上だった場合' do
+      end
+
+      it 'prefectureが選択されていない場合' do 
+      end
+
+      it 'cityが21文字以上の場合' do
+      end
+
+      it 'cityが空欄の場合' do
+      end
+
+      it 'street_addressが51文字以上の場合' do
+      end
+
+      it 'street_addressが空欄の場合' do
+      end
+
+
+      it 'descriptionが151文字以上だった場合' do
+      end
+
+      it 'total_specesが100以上だった場合' do
+      end
+
+      it 'total_spacesが数字以外だった場合' do
+      end
+
+      it 'total_spacesが0だった場合' do
+      end
+
+      it 'total_spacesが空欄だった場合' do 
+      end
+    end
+  end
 end
