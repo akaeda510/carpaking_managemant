0df38e5682e9fa8a70090c478d7d3317435fb0a6
akaeda510 <moontype510@i.softbank.jp>
2026-06-23 16:29:44 +0900

    feat:factorybot_contract_parking_spaceを作成

--- test/factories/contract_parking_spaces.rb
+++ test/factories/contract_parking_spaces.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :contract_parking_space do
-    
+    start_date { Date.today }
+    end_date { 1.month.from_now }
+    association :parking_manager
+    association :parking_space
+    association :contractor
   end
 end
