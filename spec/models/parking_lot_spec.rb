65af699c2c72cf96b31426b557eb44d2e1fb0376
akaeda510 <moontype510@i.softbank.jp>
2026-06-23 14:22:10 +0900

    feat:rspec_parking_lotのstreet_addressが51文字以上だった場合のエラーテストを実装

--- spec/models/parking_lot_spec.rb
+++ spec/models/parking_lot_spec.rb
@@ -41,6 +41,8 @@ RSpec.describe ParkingLot, type: :model do
       end
 
       it 'street_addressが51文字以上の場合' do
+        parking_lot.street_address = 'あ' * 51
+        expect(parking_lot).to be_invalid
       end
 
       it 'street_addressが空欄の場合' do
