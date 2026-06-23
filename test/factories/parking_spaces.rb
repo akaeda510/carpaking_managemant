19278704cb01bfca214c1071bfc19a1bf466f84c
akaeda510 <moontype510@i.softbank.jp>
2026-06-23 16:08:21 +0900

    update:factorybot_parking_spaceのエラーを修正

--- test/factories/parking_spaces.rb
+++ test/factories/parking_spaces.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :parking_space do
-    
+    name { Faker::Number.number(digits:2) }
+    width { Faker::Number.number(digits: 1) }
+    length { Faker::Number.number(digits: 1) }
+    price = 5000
+    status = 0
+    association :parking_area
   end
 end
