fc39d4214ea8be8aab32fbded7a19ea8a46b8e4f
akaeda510 <moontype510@i.softbank.jp>
2026-06-23 17:20:14 +0900

    update:rspec_contractorのテスト条件を修正

--- spec/models/contractor_spec.rb
+++ spec/models/contractor_spec.rb
@@ -128,10 +128,12 @@ RSpec.describe Contractor, type: :model do
         expect(contractor_1).to be_invalid
       end
 
-      it 'contact_numberが10文字以上11文字以内でなかった場合' do
+      it 'contact_numberが9文字だった場合' do
         contractor.contact_number = '1' * 9
         expect(contractor).to be_invalid
+      end
 
+      it 'contact_numberが12文字だった場合' do
         contractor.contact_number = '1' * 12
         expect(contractor).to be_invalid
       end
