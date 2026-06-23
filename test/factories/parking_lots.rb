4296cbfed73495299cea47739eb9c09da07edd0c
akaeda510 <moontype510@i.softbank.jp>
2026-06-23 13:37:56 +0900

    feat:factorybot_parking_lotを作成

--- test/factories/parking_lots.rb
+++ test/factories/parking_lots.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :parking_lot do
-    
+    name { FactoryBot::Name name }
+    prefecture { I18n.t("prefectures").values.sample }
+    city { Faker::Address.city }
+    street_address { Faker::Address.street_address }
+    total_spaces { Faker::Number.number(digits: 2).to_s }
+    association :parking_manager
   end
 end
