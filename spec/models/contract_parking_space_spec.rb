0ad77b73d35bb178648676f0a201a5c8a392b393
akaeda510 <moontype510@i.softbank.jp>
2026-06-29 13:53:20 +0900

    update:テスト条件を作成

--- spec/models/contract_parking_space_spec.rb
+++ spec/models/contract_parking_space_spec.rb
@@ -1,5 +1,97 @@
 require 'rails_helper'
 
 RSpec.describe ContractParkingSpace, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:parking_space) { FactoryBot.create(:parking_space) }
+  let(:contractor) { FactoryBot.create(:contractor) }
+
+  describe 'create' do
+    # 成功パターン
+    context 'バリデーション' do
+      it '設定した全てのバリデーションが機能しているか' do
+      end
+
+      it 'end_dateが空欄の時、"2999/12/31"になるか' do
+      end
+    end
+
+    # 失敗パターン
+    context 'バリデーション' do
+      it 'end_dateがstert_dateよりも前だった場合' do
+      end
+
+      it 'contractorが紐づいていない場合' do
+      end
+
+      it '既にparking_spaceが契約していた場合' do
+      end
+
+      it 'parking_spaceが紐づいていない場合' do
+      end
+
+      it 'parking_managerが紐づいていない場合' do
+      end
+    end
+  end
+
+  describe 'update' do
+    # 成功パターン
+    context 'バリデーション' do
+      it 'end_dateを今日に設定できるか' do
+      end
+    end
+
+    # 失敗パターン
+    context 'バリデーション' do
+      it 'start_dateを変更した場合' do
+      end
+
+      it '契約者を変更した場合' do
+      end
+
+      it '駐車スペースを変更した場合' do
+      end
+    end
+  end
+
+  describe 'スコープ' do
+    describe '.active' do
+      it '契約期間内の場合は、含まれる' do
+      end
+
+      it '契約期間外（終了済み)の場合は、含まない' do
+      end
+
+      it 'end_dateがnilで開始日が過去の場合、含まれる' do
+      end
+    end
+
+    describe '.terminated' do
+      it '終了日が過去の場合は、含まれる' do
+      end
+    end
+
+    describe '.created_this_month' do
+      it '今月作成された場合は、含まれる' do
+      end
+    end
+  end
+
+  describe '#expirigng_soon?' do
+    it '終了日が30日以内の場合は、trueを返す' do
+    end
+
+    it '終了日が31日以上先の場合、falseを返す' do
+    end
+
+    it 'end_dateが"2999/12/31"の場合、falseを返す' do
+    end
+  end
+
+  describe '#expired' do
+    it '終了日が過去の場合、trueを返す' do
+    end
+
+    it '終了日が未来の場合、falseを返す' do
+    end
+  end
 end
