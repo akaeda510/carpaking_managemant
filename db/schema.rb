785c11a6336e7a4400905bfea1d80760c23ba040
akaeda510 <moontype510@i.softbank.jp>
2026-06-30 16:16:34 +0900

    update:契約終了日を判定する専用カラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_29_043742) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_30_070147) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pg_trgm"
@@ -32,6 +32,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_29_043742) do
     t.bigint "contractor_id", null: false
     t.datetime "created_at", null: false
     t.date "end_date", default: "2999-12-31", null: false
+    t.boolean "end_date_undetermined", default: false, null: false
     t.bigint "parking_manager_id", null: false
     t.bigint "parking_space_id", null: false
     t.date "start_date", default: "1999-12-31", null: false
